5b21132ce2f08543f79d6c82c719ed31d751e928
Noah Herrin <noahherrin@Noahs-MacBook-Pro.local>
2019-06-09 22:26:48 -0700

    Implemented searching and form validation

--- js/loadmap.js
+++ js/loadmap.js
@@ -1,20 +1,47 @@
+console.log("hello world");
 var map;
-console.log("hello")
-var button = document.getElementById("searchbutton");
-button.onclick= loadmap;
-var clicked = false;
-function loadmap(){
-  if (clicked) {
-    console.log("oof")
+var button = document.getElementById("search");
+button.onclick = search;
+
+
+
+function search() {
+  console.log("hello world");
+
+
+  // parse location data
+  let lat = document.getElementById("Latitude").value;
+  let long = document.getElementById("Longitude").value;
+  let lat_dir = document.getElementById("lat_direction").value;
+  let long_dir = document.getElementById("long_direction").value;
+
+  let invalidParams = [];
+  if (lat == "" || lat > 90 || lat < -90 || lat.match(/[a-z]/i)) {
+    invalidParams.push("Latitude");
+  }
+  if (long == "" || long > 180 || long < -180 || long.match(/[a-z]/i)) {
+    invalidParams.push("Longitude");
+  }
+  if (invalidParams.length != 0) {
+    // tell user that parameters were invalid
+    console.log(invalidParams)
     return;
   }
-  var form = document.forms["LatLongSearch"]
-  let lat = form["Latitude"].value
-  let lat_dir = form["lat_direction"].value
-  console.log("Latitude: " + lat + " " + lat_dir)
-  let long = form["Longitude"].value
-  let long_dir = form["long_direction"].value
-  console.log("Longitude: " + long + " " + long_dir)
-  initMap(lat, long)
-  clicked = true;
+  lat = parseInt(lat)
+  long = parseInt(long)
+  if (lat_dir == "South") {
+    lat *= -1;
+  }
+  if (long_dir == "West") {
+    long *= -1
+  }
+  // display location on map
+  moveToLocation(lat, long);
+  placeMarker(lat, long);
+}
+
+function moveToLocation(lat, lng) {
+  var center = new google.maps.LatLng(lat, lng);
+  // using global variable:
+  map.panTo(center);
 }
